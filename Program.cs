21313358ffc40df97641ce3cf223dd0c3ad7557b
Nursultan <tukhtanazar0v.one@gmail.com>
2024-04-22 20:38:30 +0600

    Yeni Bilgilei eklemek

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+namespace Csharp{
+class Program{
+    static void Main()
+    {
+         Console.WriteLine("Hello, World !");
+         Console.WriteLine("Hello, World3!");
+         Console.ReadLine();
+    }
+}
+
+}
